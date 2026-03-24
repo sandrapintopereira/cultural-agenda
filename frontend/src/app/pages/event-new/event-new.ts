c820d112fa75e0e6545073353ca1f6253b32cb46
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-03-24 11:15:21 +0000

    feat: add necessary components and routes

---
+++ frontend/src/app/pages/event-new/event-new.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-new',
+  imports: [],
+  templateUrl: './event-new.html',
+  styleUrl: './event-new.css',
+})
+export class EventNew {}
