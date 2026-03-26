9c77547cc794330df5ec66d5e5c93128432fdb76
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-03-26 15:57:30 +0000

    feat: add logout button if login and create event only appers if the user is logged in

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  private readonly authService = inject(Auth);
+  private readonly router = inject(Router);
+
+  public user$ = this.authService.user$;
+
+  async onLogout(): Promise<void> {
+    await this.authService.logout();
+    await this.router.navigate(['/login']);
+  }
+}
--- frontend/src/app/services/auth.ts
+++ frontend/src/app/services/auth.ts
@@ -2,15 +2,29 @@ import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { environment } from '../../environments/environment';
 import { UserProfile } from '../interfaces/userProfile';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '@supabase/supabase-js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
   private supabase: SupabaseClient;
+  private _user = new BehaviorSubject<User | null>(null);
+  user$ = this._user.asObservable();
 
   constructor() {
     this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+
+    //sessão atual ao iniciar app
+    this.supabase.auth.getSession().then(({ data }) => {
+      this._user.next(data.session?.user ?? null);
+    });
+
+    //listener automático para login logout
+    this.supabase.auth.onAuthStateChange((_event, session) => {
+      this._user.next(session?.user ?? null);
+    })
   }
 
   //para registo com email e pass
@@ -25,12 +39,24 @@ export class Auth {
 
   //login com email e pass
   async login(email: string, password: string) {
-    return await this.supabase.auth.signInWithPassword({ email, password });
+    const { data, error } =
+      await this.supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+    if (data.user) {
+      this._user.next(data.user); //atualização imediata
+    }
+
+    return { data, error };
   }
 
   //logout e limpa sessão
   async logout() {
-    return await this.supabase.auth.signOut();
+    const { error } = await this.supabase.auth.signOut();
+    this._user.next(null);
+    return { error };
   }
 
   //user atual autenticado 
