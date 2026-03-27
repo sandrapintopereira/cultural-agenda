3844aefd9ad85841810bb0057b3e8a3ab4f2ae00
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-03-27 11:34:15 +0000

    feat: createEvent no service

--- frontend/src/app/services/events.ts
+++ frontend/src/app/services/events.ts
@@ -1,6 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { CreateEventDTO, Event } from '../interfaces/event';
+import { Auth } from './auth';
+import { from, Observable, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Events {}
+export class EventService {
+  private http = inject(HttpClient);
+  private auth = inject(Auth);
+  private apiUrl = `${environment.apiUrl}/events`;
+
+  createEvent(event: CreateEventDTO): Observable<Event> {
+    return from(this.auth.getToken()).pipe(
+      switchMap(token => {
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+        return this.http.post<Event>(this.apiUrl, event, { headers });
+      })
+    );
+  }
+}
